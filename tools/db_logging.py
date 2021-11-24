371ebd0d4c0896d2815e3aacb3dd1fb5d633080c
Ramón Vásquez <ramvas1984@gmail.com>
2021-11-24 15:35:41 -0300

    First commit

--- tools/db_logging.py
+++ tools/db_logging.py
@@ -4,6 +4,6 @@ from datetime import datetime
 def log(function):
     def inner(*args):
         file = open("log.txt", "a")
-        file.write(f"{str(datetime.now())} {function(*args)}" + "\n")
+        file.write(f"{str(datetime.now())} {function(*args)}\n")
 
     return inner
